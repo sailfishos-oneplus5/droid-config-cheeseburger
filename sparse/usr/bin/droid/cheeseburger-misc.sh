3ed169ea16099e24249d7e7ea24e6fc9034e641c
Jami Kettunen <jami.kettunen@protonmail.com>
2019-07-23 01:33:49 +0300

    [dcd] Fix android_storage symlinking
    Change-Id: I25d910ba0f23f607547cc4926403d216588d0ff4

--- sparse/usr/bin/droid/cheeseburger-misc.sh
+++ sparse/usr/bin/droid/cheeseburger-misc.sh
@@ -9,7 +9,8 @@ echo 1 > /sys/class/leds/button-backlight/brightness
 m_path="/data/media"
 [ -d "$m_path/0" ] && m_path+="/0"
 
-rm -f /{home/nemo/android_storage,sdcard}
-ln -s $m_path /{home/nemo/android_storage,sdcard}
+rm -f /home/nemo/android_storage /sdcard
+ln -s $m_path /home/nemo/android_storage
+ln -s $m_path /sdcard
 
 rm /var/tmp/make-droid-links

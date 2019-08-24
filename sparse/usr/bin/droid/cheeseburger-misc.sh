6745b0e80ef0b4d56b80273ee282a914acd7f6ec
Jami Kettunen <jami.kettunen@protonmail.com>
2019-08-25 01:03:41 +0300

    [dcd] Include gstreamer1.0-libav pkg & minor cheeseburger-misc improvements

--- sparse/usr/bin/droid/cheeseburger-misc.sh
+++ sparse/usr/bin/droid/cheeseburger-misc.sh
@@ -4,9 +4,10 @@
 # Enable backlight for physical buttons
 echo 1 > /sys/class/leds/button-backlight/brightness
 
-# Create links to Android Storage on first boot
+# We're done here if this isn't the first boot
 [ ! -f /var/tmp/make-droid-links ] && exit 0
 
+# Android Storage linking
 m_path="/data/media"
 [ -d "$m_path/0" ] && m_path+="/0"
 
@@ -14,4 +15,9 @@ rm -f /home/nemo/android_storage /sdcard
 ln -s $m_path /home/nemo/android_storage
 ln -s $m_path /sdcard
 
+# Misc first boot stuff
+chown -R nemo: /home/nemo/
+gpasswd -a nemo systemd-journal
+
+# Done
 rm /var/tmp/make-droid-links

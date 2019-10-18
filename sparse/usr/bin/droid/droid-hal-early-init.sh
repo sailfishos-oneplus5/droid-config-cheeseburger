24cb8ce5bd0fa25d0f646d387956cc4481d7151b
Jami Kettunen <jami.kettunen@protonmail.com>
2019-10-18 23:44:06 +0300

    [dcd] Use patched ld.config.28.txt for bionic on hybris-16.0

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -1,5 +1,12 @@
-#!/bin/env bash
+#!/bin/bash
 # Actions to execute before starting DHI.
 
-# Nothing is required (for now) so we'll just exit :)
+# A patched bionic ld.config.28.txt is required for hybris-16.0
+if ! grep -q hybris /system/etc/ld.config.28.txt; then
+	echo "using patched bionic ld.config.28.txt for hybris-16.0..."
+	mount -o bind /usr/libexec/droid-hybris/system/etc/ld.config.28.txt /system/etc/ld.config.28.txt
+	exit $?
+fi
+
+# Nothing else is required (for now) so we'll just exit :)
 exit 0

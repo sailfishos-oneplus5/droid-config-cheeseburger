ff8edbe9dcb8af0dcde1c4cbe021ee4804b17d51
Jami Kettunen <jami.kettunen@protonmail.com>
2020-11-10 19:05:09 +0200

    startup-misc: Adapt to SFOS 3.4.0 username changes
    Sailfish OS 3.4.0 changed the default username to "defaultuser" while
    upgraded installs will still be "nemo".

--- sparse/usr/bin/droid/startup-misc.sh
+++ sparse/usr/bin/droid/startup-misc.sh
@@ -8,14 +8,18 @@
 m_path="/data/android/media"
 [ -d "$m_path/0" ] && m_path+="/0"
 
+# Determine username since SFOS 3.4.0
+user="nemo"
+[ -d /home/defaultuser ] && user="defaultuser"
+
 # TODO: Start symlinking in Pictures/Android, Music/Android, ...
-rm -f /home/nemo/android_storage /sdcard
-ln -s $m_path /home/nemo/android_storage
+rm -f /home/$user/android_storage /sdcard
+ln -s $m_path /home/$user/android_storage
 ln -s $m_path /sdcard
 
 # Misc first boot stuff
-chown -R nemo: /home/nemo/
-gpasswd -a nemo systemd-journal
+chown -R $user: /home/$user/
+gpasswd -a $user systemd-journal
 
 # Done
 touch /var/tmp/made-droid-links

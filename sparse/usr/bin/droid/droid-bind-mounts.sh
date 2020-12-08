c8630335d7abe5a6525fac56e9ada895090c6941
Jami Kettunen <jami.kettunen@protonmail.com>
2020-12-08 17:43:15 +0200

    droid-bind-mounts: Add bind-mount for touch HAL init rc
    This will allow us to enable/disable sleep mode display gestures when
    the user is in the input group (i.e. nemo/defaultuser under SFOS).
    
    With this I can also stop adding said user to the droid system group
    since that was the only thing it was needed for iirc.

--- sparse/usr/bin/droid/droid-bind-mounts.sh
+++ sparse/usr/bin/droid/droid-bind-mounts.sh
@@ -8,3 +8,6 @@ mount -o bind /usr/libexec/droid-hybris/system/etc/ld.config.28.txt /system/etc/
 mount -o bind /usr/libexec/droid-hybris/vendor/etc/init/hw/init.qcom.rc /vendor/etc/init/hw/init.qcom.rc
 mount -o bind /usr/libexec/droid-hybris/vendor/etc/init/hw/init.target.performance.rc /vendor/etc/init/hw/init.target.performance.rc
 mount -o bind /usr/libexec/droid-hybris/vendor/etc/init/hw/init.target.rc /vendor/etc/init/hw/init.target.rc
+
+# Use input group ownership on /proc/touchpanel/* for SFOS gesture-daemon
+mount -o bind /usr/libexec/droid-hybris/vendor/etc/init/vendor.lineage.touch@1.0-service.oneplus_msm8998.rc /vendor/etc/init/vendor.lineage.touch@1.0-service.oneplus_msm8998.rc

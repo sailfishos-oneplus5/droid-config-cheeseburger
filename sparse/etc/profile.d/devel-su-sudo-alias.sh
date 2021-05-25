d258e90356b48d23b56ec04c603004494ad906c1
Jami Kettunen <jami.kettunen@protonmail.com>
2021-05-25 20:06:49 +0300

    Revert "profile.d: Adapt scripts to work with SFOS 4.0"
    This reverts commit 7ed095106072ff4b3d1ebb72fb3d81c5ac6e239b.

--- sparse/etc/profile.d/devel-su-sudo-alias.sh
+++ sparse/etc/profile.d/devel-su-sudo-alias.sh
@@ -1,3 +1,4 @@
 # Add convenience 'sudo' alias for 'devel-su' if sudo command isn't found
-command -v sudo > /dev/null || \
+if [[ $- = *i* && ! -x `command -v sudo` ]]; then
 	alias sudo="devel-su"
+fi
